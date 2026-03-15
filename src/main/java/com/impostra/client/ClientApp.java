a8486153d0b2608d945548164197efd6e0d8c054
Barış Sürkit <bbarisrkt@gmail.com>
2026-03-15 17:49:53 +0300

    filled the files

--- src/main/java/com/impostra/client/ClientApp.java
+++ src/main/java/com/impostra/client/ClientApp.java
@@ -1,4 +1,7 @@
 package com.impostra.client;
 
 public class ClientApp {
+    public static void main(String[] args) {
+        System.out.println("Oyuncu ekranı açılıyor...");
+    }
 }
--- src/main/java/com/impostra/server/ServerApp.java
+++ src/main/java/com/impostra/server/ServerApp.java
@@ -1,4 +1,7 @@
 package com.impostra.server;
 
 public class ServerApp {
+    public static void main(String[] args) {
+        System.out.println("Sunucu başlatılıyor...");
+    }
 }
